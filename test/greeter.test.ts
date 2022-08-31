8e1bbad57fc33489cd45065416fd133b5b61c892
Hangleang <52003343+hangleang@users.noreply.github.com>
2022-08-31 09:24:25 +0700

    Initial commit

---
+++ test/greeter.test.ts
@@ -0,0 +1,18 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { deployments, ethers } from "hardhat";
+
+import { Greeter } from "../src/types/contracts/Greeter";
+
+describe("Greeter testcase", () => {
+  it("should return the new greeting once it's changed", async function () {
+    await deployments.fixture(["Greeter"]);
+    const [deployer]: SignerWithAddress[] = await ethers.getSigners();
+
+    const greeter: Greeter = await ethers.getContract("Greeter", deployer);
+    expect(await greeter.connect(deployer).greet()).to.equal("Hello, World!");
+
+    await greeter.setGreeting("Bonjour, le monde!");
+    expect(await greeter.connect(deployer).greet()).to.equal("Bonjour, le monde!");
+  });
+});
